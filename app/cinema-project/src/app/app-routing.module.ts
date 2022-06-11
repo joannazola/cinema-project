c1b3d135516a66a9076116c196d3b902d83b250f
Joanna Zoła <joannazola97@gmail.com>
2022-06-11 22:56:36 +0200

    fix application routing

--- app/cinema-project/src/app/app-routing.module.ts
+++ app/cinema-project/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './main-page/main-page/main-page.component';
-import { WhatsOnComponent } from './whats-on/whats-on/whats-on.component';
-import { MyTicketsComponent } from './my-tickets/my-tickets/my-tickets.component';
-import { RegistrationComponent } from './registration/registration/registration.component';
-import { LoginComponent } from './login/login/login.component';
+
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
-  { path: '/', component: MainPageComponent },
-  { path: 'whats-on', component: WhatsOnComponent },
-  { path: 'my-tickets', component: MyTicketsComponent },
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'main-page', pathMatch: 'full' },
+  { path: 'main-page', component: MainPageComponent },
 ];
 
 @NgModule({
--- app/cinema-project/src/app/app.module.ts
+++ app/cinema-project/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page/main-page.component';
-import { WhatsOnComponent } from './whats-on/whats-on/whats-on.component';
-import { MyTicketsComponent } from './my-tickets/my-tickets/my-tickets.component';
-import { RegistrationComponent } from './registration/registration/registration.component';
-import { LoginComponent } from './login/login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { WhatsOnComponent } from './whats-on/whats-on.component';
+import { MyTicketsComponent } from './my-tickets/my-tickets.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +15,8 @@ import { LoginComponent } from './login/login/login.component';
     MainPageComponent,
     WhatsOnComponent,
     MyTicketsComponent,
-    RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
